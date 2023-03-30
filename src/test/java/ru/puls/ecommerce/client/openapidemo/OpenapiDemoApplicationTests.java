6b35e5c4f26b62f61a0f4bdde5cb09fcf30c13d1
Stanislav Agarkov <s.agarkov@puls.ru>
2023-03-31 01:13:04 +0300

    Initial commit

---
+++ src/test/java/ru/puls/ecommerce/client/openapidemo/OpenapiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.puls.ecommerce.client.openapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
