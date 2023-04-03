28fea750f38d210df7bbb01efa65ce4a3212d880
Stanislav Agarkov <s.agarkov@puls.ru>
2023-04-03 15:44:47 +0300

    Подключение SwaggerUI для двух разных спецификаций
    Для этого нужно:
     * замапить спецификации из ресурсов в какие-нибудь URL;
     * подключить эти спецификации в конфигурацию SwaggerUI.

---
+++ src/main/java/ru/puls/ecommerce/client/openapidemo/configuration/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package ru.puls.ecommerce.client.openapidemo.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/openapi/**")
+                .addResourceLocations("classpath:/openapi/")
+                ;
+    }
+}
